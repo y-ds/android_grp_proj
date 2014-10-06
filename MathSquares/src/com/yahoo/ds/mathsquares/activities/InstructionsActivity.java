05371c65d30684cb125e03fd46a542ba25c28f3e
Shruthi Ramesh <shruthimr@yahoo.com>
2014-10-06 13:06:45 -0700

    Instructions Activity

--- MathSquares/src/com/yahoo/ds/mathsquares/activities/InstructionsActivity.java
+++ MathSquares/src/com/yahoo/ds/mathsquares/activities/InstructionsActivity.java
@@ -1,18 +1,37 @@
 package com.yahoo.ds.mathsquares.activities;
 
-import com.yahoo.ds.mathsquares.R;
-import com.yahoo.ds.mathsquares.R.layout;
+import java.io.InputStream;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.yahoo.ds.mathsquares.R;
 
 public class InstructionsActivity extends Activity {
 
+	private ImageView sampleBoard; 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_instructions);
+
+		try 
+		{
+		    // get input stream
+		    InputStream ims = getAssets().open("math_squares.jpg");
+		    // load image as Drawable
+		    Drawable d = Drawable.createFromStream(ims, null);
+		    // set image to ImageView
+		    sampleBoard = (ImageView)findViewById(R.id.ivSampleBoard);
+		    sampleBoard.setImageDrawable(d);
+		    
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		    
 	}
 }
