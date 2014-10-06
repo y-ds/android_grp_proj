0ccffe60b845deb0e09627f0ecb2d4eac356bb1b
Shruthi Ramesh <shruthimr@yahoo.com>
2014-10-06 10:16:57 -0700

    Help Menu & LeaderboardAdapter

--- MathSquares/gen/com/yahoo/ds/mathsquares/R.java
+++ MathSquares/gen/com/yahoo/ds/mathsquares/R.java
@@ -14,17 +14,34 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int lvProblems=0x7f060000;
+        public static final int imageView1=0x7f070002;
+        public static final int lvProblems=0x7f070000;
+        public static final int miHelp=0x7f070006;
+        public static final int miLeaderboard=0x7f070005;
+        public static final int textView1=0x7f070001;
+        public static final int tvName=0x7f070003;
+        public static final int tvScore=0x7f070004;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
         public static final int activity_instructions=0x7f030001;
-        public static final int problem_item=0x7f030002;
+        public static final int activity_leaderboard=0x7f030002;
+        public static final int item_player_score=0x7f030003;
+        public static final int problem_item=0x7f030004;
+    }
+    public static final class menu {
+        public static final int settings=0x7f060000;
     }
     public static final class string {
+        public static final int User=0x7f040007;
+        public static final int User1Score=0x7f040008;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int help=0x7f040005;
+        public static final int leaderboard=0x7f040004;
+        public static final int rules=0x7f040006;
         public static final int title_activity_instructions=0x7f040002;
+        public static final int title_activity_leaderboard=0x7f040003;
     }
     public static final class style {
         /** 
--- MathSquares/src/com/yahoo/ds/mathsquares/activities/GameActivity.java
+++ MathSquares/src/com/yahoo/ds/mathsquares/activities/GameActivity.java
@@ -1,8 +1,11 @@
 package com.yahoo.ds.mathsquares.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.parse.LogInCallback;
 import com.parse.ParseAnonymousUtils;
@@ -14,6 +17,8 @@ public class GameActivity extends Activity {
 	
     private static final String TAG = GameActivity.class.getName();
     private static String sUserId;
+    private static final Integer INSTRUCTIONS_INTENT = 5;
+    private static final Integer LEADERBOARD_INTENT = 6;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,32 @@ public class GameActivity extends Activity {
         } else { // If not logged in, login as a new anonymous user
             login();
         }
+	}
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+	}
+	
+	
+	public void onSettingsLeaderboard(MenuItem mi)
+	{
+    	Intent i = new Intent(this, LeaderboardActivity.class);
+    	//Pass arguments
+    	//Execute Intent startActivityForResults
+    	startActivityForResult(i, LEADERBOARD_INTENT);
+	}
+	
+	public void onSettingsHelp(MenuItem mi)
+	{
+    	Intent i = new Intent(this, InstructionsActivity.class);
+    	//Pass arguments
+    	//Execute Intent startActivityForResults
+    	startActivityForResult(i, INSTRUCTIONS_INTENT); 
 	}
 	
 	private void startWithCurrentUser() {
